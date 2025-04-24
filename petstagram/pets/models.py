27b21366f6ca5080eb3733b4d3d77dcd6e21f5cd
dennie <denitsa.sureva@icloud.com>
2025-04-24 12:01:11 +0200

    fix: updated pet model - slug is not editable and updated save method

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -5,10 +5,11 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(null=False, blank=True, unique=True)
+    slug = models.SlugField(null=True, blank=True, unique=True, editable=False)
 
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) #it is called twice, to get the id first and then to save the combination with self.name
         if not self.slug:
             self.slug = slugify((f"{self.name}-{self.id}"))
         super().save(*args, **kwargs)
