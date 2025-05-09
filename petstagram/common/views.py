036b1f2f1c2976f8357ce14853c14bf5ec8b8c7b
dennie <denitsa.sureva@icloud.com>
2025-05-09 14:55:05 +0200

    feat:searchbar

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect, resolve_url
 from petstagram.photos.models import Photo
 from petstagram.common.models import Like
-from petstagram.common.forms import CommentForm
+from petstagram.common.forms import CommentForm, SearchForm
 from pyperclip import copy
 # Create your views here.
 def home_page(request):
     all_photos = Photo.objects.all()
     comment_form = CommentForm()
+    search_form = SearchForm(request.GET)
+
+    if search_form.is_valid():
+        all_photos = all_photos.filter(
+            tagged_pets__name__icontains=search_form.cleaned_data['pet_name']
+        )
     context = {
         'all_photos': all_photos,
         'comment_form': comment_form,
+        'search_form': search_form,
     }
 
     return render(request, 'common/home-page.html', context)
