2be5b6e24c8c1c35f2daf5f9759ced6cf311d5b1
dennie <denitsa.sureva@icloud.com>
2025-06-05 19:43:49 +0200

    logout page

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from petstagram.accounts import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register/', views.AppUserRegisterView.as_view(), name='register'),
     path('login/', views.AppUserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('profile/<int:pk>', include([
         path('', views.profile_details, name='profile-details'),
